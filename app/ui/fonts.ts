a3be91a9f6115a398bd4f5502dcc2fa08eeca795
wilonweb@gmail.com <wilonweb@gmail.com>
2024-05-09 19:01:00 +0200

    test

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'], // Choix courants pour la police Lusitana
+  subsets: ['latin'], // Sous-ensemble pour les caractères latins
+});
